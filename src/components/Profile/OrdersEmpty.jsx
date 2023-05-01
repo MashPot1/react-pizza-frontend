ba2ef3c84602f124f2f7c5a66c94f4f7d0dbedd3
Roman Muromtsev <64442434+MashPot1@users.noreply.github.com>
2023-05-01 16:09:59 +0600

    Update OrdersEmpty.jsx

--- src/components/Profile/OrdersEmpty.jsx
+++ src/components/Profile/OrdersEmpty.jsx
@@ -3,23 +3,17 @@ import { Link } from "react-router-dom";
 
 const OrdersEmpty = () => {
   return (
-    <div className="content">
-      <div className="container">
-        <div className="content__top"></div>
-        <h2 className="content__title">Мои заказы</h2>
-        <div className="cart cart--empty">
-          <h2>
-            Вы ещё ничего не заказывали <icon>😕</icon>
-          </h2>
-          <p style={{ marginBottom: "10vh" }}>
-            Вероятней всего, вы не заказывали ещё пиццу. Для того, чтобы
-            заказать пиццу, перейди на главную страницу.
-          </p>
-          <Link to="/" className="button button--black">
-            <span>Вернуться назад</span>
-          </Link>
-        </div>
-      </div>
+    <div className="cart cart--empty">
+      <h2>
+        Вы ещё ничего не заказывали <icon>😕</icon>
+      </h2>
+      <p style={{ marginBottom: "10vh" }}>
+        Вероятней всего, вы не заказывали ещё пиццу. Для того, чтобы заказать
+        пиццу, перейди на главную страницу.
+      </p>
+      <Link to="/" className="button button--black">
+        <span>Вернуться назад</span>
+      </Link>
     </div>
   );
 };
